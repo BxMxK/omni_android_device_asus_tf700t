26f268c0f6c93a6200beec7f3f0d4fb50cce8ac2
Josh <jl52741@gmail.com>
2014-09-03 18:19:25 -0500

    making the build happy

--- device.mk
+++ device.mk
@@ -4,7 +4,7 @@
 # you may not use this file except in compliance with the License.
 # You may obtain a copy of the License at
 #
-#      http://www.apache.org/licenses/LICENSE-2.0
+# http://www.apache.org/licenses/LICENSE-2.0
 #
 # Unless required by applicable law or agreed to in writing, software
 # distributed under the License is distributed on an "AS IS" BASIS,
@@ -12,18 +12,16 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-# Inherit from those products. Most specific first.
-$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
+# call the proprietary setup
+$(call inherit-product-if-exists, vendor/asus/tf700t/tf700t-vendor.mk)
 
-# Inherit from our custom product configuration
-$(call inherit-product, vendor/zombi/config/common_tablet.mk)
+# Inherit from transformer-common
+$(call inherit-product, device/asus/transformer-common/device.mk)
 
-# Inherit from those products. Most specific first.
+# local overlays
+DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
-$(call inherit-product, device/asus/tf700t/device.mk)
-
-# Discard inherited values and use our own instead.
-PRODUCT_NAME := zombi_tf700t
-PRODUCT_DEVICE := tf700t
-PRODUCT_BRAND := Asus
-PRODUCT_MODEL := TF700T
+# Build characteristics setting 
+PRODUCT_CHARACTERISTICS := tablet
+PRODUCT_AAPT_CONFIG := normal mdpi hdpi xhdpi
+PRODUCT_AAPT_PREF_CONFIG := hdpi
